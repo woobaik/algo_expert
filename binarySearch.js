5031b65d94f7300e4fd2baa8934697b34f5813e3
Joungwoo Baik <joungwoo.baik@gmail.com>
2022-06-22 02:55:36 +0900

    Finish binary search

--- binarySearch.js
+++ binarySearch.js
@@ -1,18 +1,19 @@
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arr = [1, 2, 3, 4, 5, 6, 8, 9, 10];
 
 const binarySearch = (arr, target) => {
   let left = 0;
   let right = arr.length - 1;
+  if (target > arr[right] || target < arr[left]) {
+    return false;
+  }
 
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
-    console.log("left", left);
-    console.log("mid", mid);
-    console.log("right", right);
+
     if (arr[mid] < target) {
-      left = mid;
+      left = mid + 1;
     } else if (arr[mid] > target) {
-      right = mid;
+      right = mid - 1;
     } else {
       return true;
     }
